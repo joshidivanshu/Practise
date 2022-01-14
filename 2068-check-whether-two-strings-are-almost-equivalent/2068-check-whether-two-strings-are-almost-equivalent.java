a239dc2794338b6568103c3128f0391b487ea19a
Divanshu Joshi <joshidivanshu16@gmail.com>
2022-01-14 12:45:29 +0530

    Time: 2 ms (45.27%), Space: 39.2 MB (9.20%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
-        int arr1[] = new int[256];
-        int arr2[] = new int[256];
-        int n1 = word1.length();
-        int n2 = word2.length();
-        for(int i=0;i<n1;i++)
-            arr1[word1.charAt(i)]++;
-        for(int i=0;i<n2;i++)
-            arr2[word2.charAt(i)]++;
-        for(int i=0;i<256;i++)
+        int arr[] = new int[256];
+        for(int i=0;i<word1.length();i++)
         {
-            if(Math.abs(arr1[i]-arr2[i]) > 3)
+            arr[word1.charAt(i)]++;
+            arr[word2.charAt(i)]--;
+        }
+        for(int l : arr)
+        {
+            if(l < -3 || l >3)
                 return false;
         }
         return true;
