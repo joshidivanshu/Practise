cedf3fcba4e1b5a8049f2f41b1a68a653e0fa41c
Divanshu Joshi <joshidivanshu16@gmail.com>
2022-02-18 18:18:18 +0530

    Time: 421 ms (23.11%), Space: 148.5 MB (5.72%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,16 +1,16 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        if(n == 1 || n == 0)
+        int dp[][] = new int[n+1][n+1];
+        //do[i][j] denotes if palindromic with length i ending at j.
+        if(n == 0 || n == 1)
             return s;
         if(n == 2)
         {
             if(s.charAt(0) == s.charAt(1))
                 return s;
-            return Character.toString(s.charAt(0));
+            return String.valueOf(s.charAt(0));
         }
-        int dp[][] = new int[n+1][n+1];
-        //dp[i][j] represents string of length i ending at index j
         for(int i=0;i<=n;i++)
         {
             dp[0][i] = 1;
@@ -27,24 +27,22 @@ class Solution {
                 {
                     dp[i][j] = dp[i-2][j-1];
                 }
-                else
-                    dp[i][j] = 0;
             }
         }
         int len = 1;
         int endingAt = 1;
-        for(int i=1;i<=n;i++)
+        for(int i=2;i<=n;i++)
         {
             for(int j=1;j<=n;j++)
             {
-                if(dp[i][j] == 1 && i>len)
+                if(dp[i][j] == 1 && i > len)
                 {
                     len = i;
                     endingAt = j;
                 }
             }
-            
         }
         return s.substring(endingAt-len,endingAt);
+        
     }
 }
