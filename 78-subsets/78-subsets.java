a52366727874b7c53ce30425ab8da1c7f1a472f4
Divanshu Joshi <joshidivanshu16@gmail.com>
2022-03-08 09:29:25 +0530

    Time: 1 ms (82.22%), Space: 43.9 MB (9.90%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -2,20 +2,18 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         int n = nums.length;
-        List<Integer> path = new ArrayList<Integer>();
-        backtrack(nums,res,path,0);
+        List<Integer> set = new ArrayList<Integer>();
+        dfs(res,set,nums,0);
         return res;
     }
-    
-    public static void backtrack(int[] nums,List<List<Integer>> res, List<Integer> path, int s)
+    public void dfs(List<List<Integer>> res, List<Integer> set, int[] nums,int ind)
     {
-        res.add(new ArrayList<Integer>(path));
-        for(int i=s;i<nums.length;i++)
+        res.add(new ArrayList<Integer>(set));
+        for(int i=ind;i<nums.length;i++)
         {
-            path.add(nums[i]);
-            backtrack(nums,res,path,i+1);
-            path.remove(path.size()-1);
+            set.add(nums[i]);
+            dfs(res,set,nums,i+1);
+            set.remove(set.size()-1);
         }
-        return;
     }
 }
