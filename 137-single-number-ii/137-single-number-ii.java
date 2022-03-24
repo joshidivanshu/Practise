112f0547249b36b8e5ed7155c92f7b86d73e0a63
Divanshu Joshi <joshidivanshu16@gmail.com>
2022-03-24 21:24:32 +0530

    Time: 4 ms (63.25%), Space: 44 MB (54.39%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -1,24 +1,24 @@
 class Solution {
     public int singleNumber(int[] arr) {
-        int result = 0;
-        int x, sum;
+        int res = 0;
+        int x;
         int n = arr.length;
- 
-        // Iterate through every bit
-        for (int i = 0; i < 32; i++) {
-            // Find sum of set bits at ith position in all
-            // array elements
-            sum = 0;
-            x = (1 << i);
-            for (int j = 0; j < n; j++) {
-                if ((arr[j] & x) != 0)
-                    sum++;
+        for(int i=0;i<32;i++)
+        {
+            x = 1<<i;
+            int cnt = 0;
+            for(int j=0;j<n;j++)
+            {
+                if((arr[j]&x) != 0)
+                {
+                    cnt++;
+                }
             }
-            // The bits with sum not multiple of 3, are the
-            // bits of element with single occurrence.
-            if ((sum % 3) != 0)
-                result |= x;
+            if(cnt%3 != 0)
+            {
+                res |= x;
+            }
         }
-        return result;
+        return res;
     }
 }
