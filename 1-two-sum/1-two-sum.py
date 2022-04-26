40722d90d5c3889fed07ff5a2a7ddc47b35212a3
Divanshu Joshi <joshidivanshu16@gmail.com>
2022-04-26 12:10:28 +0530

    Time: 51 ms (81.40%), Space: 14.1 MB (95.47%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,21 +1,14 @@
-class Solution {
-    public int[] twoSum(int[] arr, int target) {
-        int res[] = new int[2];
-        int n = arr.length;
-        Map<Integer,Integer> hm = new HashMap<Integer, Integer>();
-        for(int i=0;i<n;i++)
-        {
-            if(hm.containsKey(target-arr[i]))
-            {
-                res[0] = i;
-                res[1] = hm.get(target-arr[i]);
-                return res;
-            }
-            else
-            {
-                hm.put(arr[i], i);
-            }
-        }
-        return res;
-    }
-}
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map = {}
+        for i in range(len(nums)):
+            if target-nums[i] in map:
+                return map[target-nums[i]],i
+            map[nums[i]] = i
+        return -1,-1    
+        
